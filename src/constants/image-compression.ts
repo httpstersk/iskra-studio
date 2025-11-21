5619057d97821e3007b3b6e46fc42bedf06e5ce8
Lubos Belak <belak.lubos@gmail.com>
2025-11-21 20:57:58 +0100

    ⚡ perf: Increase maximum image dimensions from 1920x1080 to 4096x4096 for higher quality uploads

--- src/constants/image-compression.ts
+++ src/constants/image-compression.ts
@@ -9,13 +9,13 @@
  * Maximum image width in pixels for uploads.
  * Images will be scaled down to fit within this width while maintaining aspect ratio.
  */
-export const MAX_IMAGE_WIDTH = 1920;
+export const MAX_IMAGE_WIDTH = 4096;
 
 /**
  * Maximum image height in pixels for uploads.
  * Images will be scaled down to fit within this height while maintaining aspect ratio.
  */
-export const MAX_IMAGE_HEIGHT = 1080;
+export const MAX_IMAGE_HEIGHT = 4096;
 
 /**
  * JPEG compression quality (0-1).
